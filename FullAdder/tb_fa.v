68a5ef44305ee79848edd6cc10de70c3235272f8
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2024-02-06 14:59:16 +0530

    Update tb_fa.v

--- FullAdder/tb_fa.v
+++ FullAdder/tb_fa.v
@@ -2,7 +2,7 @@ module tb_fa;
   reg a, b, c;
   wire s, c_out;
   
-  fulladder fa(a, b, c, s, c_out); //change to fa_ha fa(a,b,c,s,c_out); if you're using full adder using 2 half adders for testbench
+  fulladder fa(a, b, c, s, c_out); //change to fa_ha fa(a, b, c, s, c_out); if you're using full adder using 2 half adders for testbench
   
   initial begin
     $monitor("At time %0t, a=%b ,b=%b, cin=%b, sum=%b, carry=%b",$time, a,b,c,s,c_out);
