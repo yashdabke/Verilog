14b9dc0c9cfb1ff7777ef72276afe46ed96ccfcb
Yash Dabke <112268819+yashdabke@users.noreply.github.com>
2024-02-07 12:45:08 +0530

    Update multiplier_tb.v

--- Multiplier/multiplier_tb.v
+++ Multiplier/multiplier_tb.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.02.2024 15:24:00
-// Design Name: 
-// Module Name: multiplier_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+//testbench for mul.v 
 
 module multiplier_tb;
    parameter n_bits = 5;
